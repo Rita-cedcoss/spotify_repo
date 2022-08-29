f670b42c3c51f44a0dec3c98c519a07d8fac1c6f
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-29 14:45:15 +0530

    component 2 task completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Component1 from './SpotifyComponent/Component1';
 import Component2 from './SpotifyComponent/Component2';
 function App() {
   return (
     <>
-    <Component1></Component1>
     <Component2></Component2>
     </>
   );
