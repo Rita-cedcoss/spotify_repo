7de04919e13356a9d3ec043c9f9c1631d6902825
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-29 10:57:57 +0530

    component 1 task completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-// import SpotifyComponent from './SpotifyComponent/SpotifyComponent';
 import Component1 from './SpotifyComponent/Component1';
-
 function App() {
   return (
-    // <SpotifyComponent></SpotifyComponent>
     <Component1></Component1>
   );
 }
-
 export default App;
